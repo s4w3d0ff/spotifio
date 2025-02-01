f6be0a4b6be3ee1d87e11d65c47aad3f8783d317
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2025-02-01 14:02:56 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="spotifio",
-    version="0.1.0",
+    version="0.1.1",
     author="s4w3d0ff",
     author_email="",
     description="An async Spotify Web API client library",
